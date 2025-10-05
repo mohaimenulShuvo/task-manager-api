33e59110d892502ebdcc66042bba066aa31bebe0
mohaimenulShuvo <mohaimenulshuvo90@gmail.com>
2025-10-05 22:53:28 +1100

    app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: "Hello from Task Manager API" });
+});
+
 const express = require('express');
 const mongoose = require('mongoose');
 const taskRoutes = require('./routes/taskRoutes');
